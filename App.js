a735ed9ce7f34da03f87423245c98ba9fed7b6a1
Manasa Ponaganti <manasa.ponaganti@SG-LM-005.local>
2024-02-17 13:28:55 +0530

    episode-3

--- App.js
+++ App.js
@@ -31,16 +31,50 @@ import ReactDOM from "react-dom/client";
 }
 // writing above structure in react
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "Im h1 tag in child1"),
-    React.createElement("h2", {}, "Im h2 tag in child1"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "Im h1 tag in child2"),
-    React.createElement("h2", {}, "Im h2 tag in child2"),
-  ]),
-]);
+// const parent = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child1" }, [
+//     React.createElement("h1", {}, "Im h1 tag in child1"),
+//     React.createElement("h2", {}, "Im h2 tag in child1"),
+//   ]),
+//   React.createElement("div", { id: "child2" }, [
+//     React.createElement("h1", {}, "Im h1 tag in child2"),
+//     React.createElement("h2", {}, "Im h2 tag in child2"),
+//   ]),
+// ]);
+
+//JSX
+
+// const jsxHeading = (
+//   <h1 className="heading" tabIndex={1}>
+//     Hi JSX Code
+//   </h1>
+// );
+
+//Fucntional Component
+
+//It is functional components which is also a normal js arrow function
+const Title = () => <h1>Title</h1>;
+
+const  number=6789;
+
+const HeadingComponent = () => {
+  return (
+    <div>
+      {/* Treating Title as normal function(JS Code)  */}
+      {Title()}
+
+      {/* Treating Title as component which is also JS code */}
+      {<Title/>}
+
+      {/* Any Js Expression(JS Code) can be written like this */}
+      {number}
+
+      {/* Component composition i.e., component inside component*/}
+      <Title/>
+      <h2>This is heading Components</h2>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent />);
